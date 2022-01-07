8222ad62f64a9c590af7fcac9db3d13b1a3a4648
Jared Jewell <jaje9434@colorado.edu>
2022-01-07 09:55:08 -0700

    add tailwind
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import "../styles/Home.module.css";
+
+export default function Home() {
+  return <h1 className="text-xl font-bold text-black">hello, world</h1>;
+}
