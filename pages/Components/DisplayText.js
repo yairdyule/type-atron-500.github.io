6259b29e181ef96612f007e0ab590456753eead4
Jared Jewell <jaje9434@colorado.edu>
2022-01-08 12:28:32 -0700

    create display txt component
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

---
+++ pages/Components/DisplayText.js
@@ -0,0 +1,34 @@
+export default function DisplayText({ input, target, focused }) {
+  return (
+    <p
+      className={
+        "    font-bold tracking-widest ease-in-out duration-150" +
+        (!focused && " blur-sm")
+      }
+    >
+      {input.split("").map((char, index) => {
+        let className;
+        if (char == target[index]) {
+          className = "text-emerald-300";
+        } else if (char != target[index]) {
+          className = "text-red-300";
+        }
+        return (
+          <span key={index} className={className}>
+            {char}
+          </span>
+        );
+      })}
+      {target
+        .split("")
+        .slice(input.length)
+        .map((char, ind) => {
+          return (
+            <span key={ind} className="text-slate-400">
+              {char}
+            </span>
+          );
+        })}
+    </p>
+  );
+}
