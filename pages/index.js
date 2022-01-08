db941b6fc5a49526e5d33695b1605213846cf387
Jared Jewell <jaje9434@colorado.edu>
2022-01-08 12:48:38 -0700

    hardcode data for now - can't get it to work
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import "../styles/Home.module.css";
 import TypingBox from "./Components/TypingBox";
-import { texts } from "../public/data";
 
 export default function Home() {
+  const texts = [
+    "oh no, are you going to finish that?",
+    "my though as well",
+    "the quick brown fox jumped over the lazy dog",
+    "hello, world!",
+  ];
+
   const target = texts[Math.floor(Math.random() * texts.length)].text;
 
   return (
