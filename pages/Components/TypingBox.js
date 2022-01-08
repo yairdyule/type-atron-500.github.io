495aaff62eae187e337ef1d57ec5a44515063caf
Jared Jewell <jaje9434@colorado.edu>
2022-01-08 12:29:02 -0700

    create input component
    Signed-off-by: Jared Jewell <jaje9434@colorado.edu>

---
+++ pages/Components/TypingBox.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import DisplayText from "./DisplayText";
+
+export default function TypingBox({ target }) {
+  const [input, setInput] = useState("");
+  const [isFocused, setFocused] = useState(false);
+
+  return (
+    <div className="m-12 relative ">
+      {!isFocused && (
+        <p className=" text-slate-200">focus whenever you're ready</p>
+      )}
+      <textarea
+        onChange={(e) => setInput(e.target.value)}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        value={input}
+        spellCheck="false"
+        className="bg-transparent top-1/2 left-1/2 -translate-x-1/3 -translate-y-8 border-slate-50 border-4 text-transparent absolute resize-none outline-none w-96 h-56"
+      ></textarea>
+      <DisplayText input={input} target={target} focused={isFocused} />
+    </div>
+  );
+}
