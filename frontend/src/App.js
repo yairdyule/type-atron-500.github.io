cef1020ad2500d5a5db79ec1bc687e8f9e1c3129
Jared Jewell <jaje9434@colorado.edu>
2022-01-24 21:52:20 -0700

    clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import "./App.css";
-import Login from "./components/buttons/Login";
-import TypingBox from "./components/TypingBox";
-import AddText from "./components/AddText";
+import { Outlet } from "react-router-dom";
+
+import Nav from "./components/layout/Nav";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    async function getData() {
-      let data = await axios.get("http://localhost:8000");
-      console.log(data.data);
-    }
-    getData();
-  }, []);
   return (
-    <div className="flex flex-col items-center justify-center">
-      {!loggedIn && <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
-      <TypingBox />
-      <AddText />
-    </div>
+    <>
+      <Nav />
+      <Outlet />
+    </>
   );
 }
 
